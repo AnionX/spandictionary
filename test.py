5d87ad6d8dcc3ef7ca27650af45642e8932236d8
AnionX <seth.morris@northidahochristianschool.com>
2017-03-21 15:00:14 -0700

    updated to my version
    my actual project starts

--- test.py
+++ test.py
@@ -1,26 +1,27 @@
-words = {"Yeet":"Diabetes","Neap":"A type of spring tide"}
-word=""
-definition=""
-choice=""
+spnsh = {"mirar":"to watch","sacar":"to take","esperar":"to wait or to hope","invitar":"to invite","buscar":"to look for","predicar":"to preach","ensenar":"to teach","tocar":"to play(intrument,etc.)","jugar":"to play(game,etc.)","senular":"to signal","reservar":"to reserve"}
+word = ""
+definiton = ""
+choice = ""
 while True:
-    print("enter quit to quit")
-    choice=input()
+    print("enter 'quit' to quit")
+    choice = input()
     if choice=="new":
-        print("Please enter the word you'd wish to add.")
-        word=input()
-        print("Please enter the definition for "+word)
-        definition=input()
-        words[word]=definition
+        print("Please enter the word you wish to add")
+        word = input()
+        print("Please enter the definition of "+word)
+        definition = input()
+        spnsh[word] = [definition]
     elif choice=="print":
-        for i in words:
+        for i in spnsh:
             print(i)
     elif choice=="definition":
-        print("Which word would you like the definition of")
-        word=input()
-        definition=words[word]
+        print("Which word would you like the definition of?")
+        word = input()
+        definition = spnsh[word]
         print(definition)
     elif choice=="quit":
         break
     else:
         print("Invalid option")
+        
 
