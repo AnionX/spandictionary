c4d4beaa530d47f74db7ca7e145cd51b8fd914c5
AnionX <seth.morris@northidahochristianschool.com>
2017-04-04 15:40:57 -0700

    finished stuff and update option

--- test.py
+++ test.py
@@ -1,22 +1,42 @@
-gradebook = {"Seth Morris":"A"}
+gradebook = {"Seth Morris":{"HW1":"100","HW2":"98","HW3":"97"}}
 student = ""
 assignment = ""
+assignments = {}
 grade = ""
 choice = ""
 while True:
+    print("new - new students and assignments")
+    print("print - print students")
+    print("assignments - print assignments of a student")
+    print("update - update an existing grade")
     print("enter 'quit' to quit")
     choice = input()
     if choice=="new":
-        print("Please enter name of the student you wish to add")
-        student = input()
-        print("Please enter the assignment for "+student)
-        assignment = input()
-        gradebook[student] = [assignment]
+        print("New student or new assignment?")
+        choice = input()
+        if choice=="student":
+            print("Please enter the student you would like to add.")
+            student = input()
+            gradebook[student]={}
+        elif choice=="assignment":
+            print("Please enter the name of the assignment.")
+            assignment = input()
+            print("Which student would you like to add this assignment to?")
+            student = input()
+            print("What is the grade for "+assignment)
+            gradebook[student][assignment] = input()
     elif choice=="print":
         for i in gradebook:
             print(i)
+    elif choice=="update":
+        print("Whose grade would you like to update?")
+        student = input()
+        print("Which assignment do you want to update?")
+        assignment = input()
+        print("What is the new grade for "+assignment)
+        gradebook[student][assignment] = input()
     elif choice=="grade":
-        print("Which student's grade you like?")
+        print("Which student's grade would you like?")
         student = input()
         grade = gradebook[student]
         print(grade)
